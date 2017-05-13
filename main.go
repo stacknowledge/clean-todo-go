a8d135a8266d60db6d1d64dc55452f44758f258f
Pedro Fernandes <stacknowledge@gmail.com>
2017-05-13 05:00:04 +0100

    Added purpose todo interactor, todo storage injection and todo routes

--- main.go
+++ main.go
@@ -7,14 +7,22 @@ import (
 
 	"strconv"
 
+	"fmt"
+
+	"time"
+
 	"github.com/gorilla/mux"
 	"github.com/stacknowledge/go-clean-todo/src/domain"
-	database "github.com/stacknowledge/go-clean-todo/src/infrastructure/database"
+	"github.com/stacknowledge/go-clean-todo/src/infrastructure"
+	"github.com/stacknowledge/go-clean-todo/src/infrastructure/database"
 	"github.com/stacknowledge/go-clean-todo/src/interfaces"
 	"github.com/stacknowledge/go-clean-todo/src/usecases"
 )
 
+var userId uint
+
 func main() {
+
 	dbHandler, err := database.NewSqliteHandler("./database.sqlite")
 	router := mux.NewRouter()
 
@@ -22,27 +30,52 @@ func main() {
 		log.Fatal(err)
 	}
 
-	handlers := make(map[string]database.DbHandler)
+	handlers := make(map[string]infrastructure.DbHandler)
 	handlers["UserRepository"] = dbHandler
+	handlers["TodoRepository"] = dbHandler
 
 	userInteractor := new(usecases.UserInteractor)
 	userInteractor.UserRepository = interfaces.NewDbUserRepository(handlers)
 
-	router.HandleFunc("/user", func(res http.ResponseWriter, req *http.Request) {
-		_, error := userInteractor.Register(domain.User{
+	todoInteractor := new(usecases.TodoInteractor)
+	todoInteractor.TodoRepository = interfaces.NewDbTodoRepository(handlers)
+
+	router.HandleFunc("/user/register", func(res http.ResponseWriter, req *http.Request) {
+		userId, error := userInteractor.Register(domain.User{
 			Name:     "Peters",
-			Email:    "peters@badass.com",
+			Email:    "petersx@badass.com",
 			Password: "petersboysband",
 		})
 
 		if error != nil {
 			panic(error)
 		}
+
+		log.Println(fmt.Sprintf("User added ID {%d}", userId))
 	})
 
 	router.HandleFunc("/user/{id}", func(res http.ResponseWriter, req *http.Request) {
 		id, _ := strconv.Atoi(mux.Vars(req)["id"])
-		panic(userInteractor.Profile(uint(id)))
+		log.Println(fmt.Sprintf("User profile: {%v}", userInteractor.Profile(uint(id))))
+	})
+
+	router.HandleFunc("/todo/create", func(res http.ResponseWriter, req *http.Request) {
+		todoId, err := todoInteractor.Store(&domain.Todo{
+			Title:   "O todo que a gente faz whateevveerssss",
+			Expires: time.Now().Format("02-01-2006 03:04:05"),
+			User:    userInteractor.Profile(userId),
+		})
+
+		if err != nil {
+			panic(err)
+		}
+
+		log.Println(fmt.Sprintf("Todo added ID {%d}", todoId))
+	})
+
+	router.HandleFunc("/todo/{id}", func(res http.ResponseWriter, req *http.Request) {
+		id, _ := strconv.Atoi(mux.Vars(req)["id"])
+		log.Println(fmt.Sprintf("Todo: {%v}", todoInteractor.FindById(uint(id))))
 	})
 
 	http.Handle("/", router)
