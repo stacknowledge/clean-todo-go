24adf42d0f95019c1fba4931bd8a2260a77855e2
Pedro Fernandes <stacknowledge@gmail.com>
2017-05-13 04:59:50 +0100

    Updated database migrations and sqlite binary

--- create_tables.sql
+++ create_tables.sql
@@ -1,6 +1,14 @@
 CREATE TABLE `users` (
     `id` INTEGER PRIMARY KEY AUTOINCREMENT,
-    `name` VARCHAR(64) NULL,
+    `name` VARCHAR(64) NOT NULL,
     `email` VARCHAR(64) UNIQUE,
-    `password` VARCHAR(64) NULL
+    `password` VARCHAR(64) NOT NULL
+);
+
+CREATE TABLE `todos` (
+    `id` INTEGER PRIMARY KEY AUTOINCREMENT,
+    `title` VARCHAR(120) UNIQUE,
+    `expires_at` VARCHAR(20) NOT NULL,
+    `user_id` INTEGER NOT NULL,
+    FOREIGN KEY(user_id) REFERENCES users(id)
 );
